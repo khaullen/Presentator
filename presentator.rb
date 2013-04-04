283c05d189f1e445f938637c1e0ea7c571b1507f
Colin Regan <colin@redcup.la>
2013-04-04 01:33:02 -0400

    Got timer working with data from server

--- presentator.rb
+++ presentator.rb
@@ -15,11 +15,20 @@ class Presentation
 	property :time_allotted, Integer, :default => 120
 	property :presentation_date, Date, :required => true
 	property :complete, Boolean, :required => true, :default => false
-	property :created_at, DateTime
-	property :updated_at, DateTime
+	property :created_at, Time
+	property :updated_at, Time
+	property :in_progress, Boolean, :required => true, :default => false
+	property :started_at, Time
 end
 
 DataMapper.finalize.auto_upgrade!
+
+class Presentation
+	def finish_at
+		#@started_at && @started_at + @time_alloted
+		(Time.now + @time_allotted).strftime('%s')
+	end
+end
 
 helpers do
 	include Rack::Utils
@@ -52,7 +61,7 @@ post '/' do
 	p.topic = params[:topic]
 	p.presenter = params[:presenter]
 	p.link = params[:link]
-	p.created_at = p.updated_at = DateTime.now
+	p.created_at = p.updated_at = Time.now
 	p.presentation_date = Date.today
 	p.save
 	redirect '/'
--- public/js/timer.js
+++ public/js/timer.js
@@ -1,26 +1,75 @@
-$(function() {
-// 	In practice we need to request endTimeStamp from server
-	var endTimeStamp = 1464516955000;
-	var timer = $('<p class="timer"></p>');
-	$(".topic").eq(0).after(timer);
-	
-	var timeFunction = function() {
-		var currentTime = new Date();
-		var totalSeconds = parseInt((endTimeStamp - currentTime.getTime()) / 1000);
-		var hours = parseInt(totalSeconds / 3600) % 24;
-		var minutes = parseInt(totalSeconds / 60) % 60;
-		var seconds = totalSeconds % 60;
-		
-		var timeArr = [];
-		if (hours) {
-			timeArr.push(hours < 10 ? "0" + hours : hours);
-		}
-		timeArr.push(minutes < 10 ? "0" + minutes : minutes);
-		timeArr.push(seconds < 10 ? "0" + seconds : seconds);
-		
-		timer.html(timeArr.join(":"));
+// $(function() {
+// // 	In practice we need to request endTimeStamp from server
+// 	var endTimeStamp = 1464516955000;
+// 	var timer = $('<p class="timer"></p>');
+// 	$(".topic").eq(0).after(timer);
+// 	
+// 	var timeFunction = function() {
+// 		var currentTime = new Date();
+// 		var totalSeconds = parseInt((endTimeStamp - currentTime.getTime()) / 1000);
+// 		var hours = parseInt(totalSeconds / 3600) % 24;
+// 		var minutes = parseInt(totalSeconds / 60) % 60;
+// 		var seconds = totalSeconds % 60;
+// 		
+// 		var timeArr = [];
+// 		if (hours) {
+// 			timeArr.push(hours < 10 ? "0" + hours : hours);
+// 		}
+// 		timeArr.push(minutes < 10 ? "0" + minutes : minutes);
+// 		timeArr.push(seconds < 10 ? "0" + seconds : seconds);
+// 		
+// 		timer.html(timeArr.join(":"));
+// 	};
+// 	
+// 	timeFunction();
+// 	setInterval(timeFunction, 1000);
+// })
+
+
+
+
+
+
+
+
+
+
+
+var Timer = (function () {
+	var timer = function (article, options) {
+		this.options = options || {};
+		this.countdownString = article.getElementsByClassName('countdownString')[0];
+		this.trigger = article.getElementsByClassName('trigger')[0];
+		this.endTime = this.countdownString.getAttribute('end-time');
 	};
 	
-	timeFunction();
-	setInterval(timeFunction, 1000);
-})
+	timer.prototype = {
+		constructor: timer,
+		setTime: function () {
+			var currentTime = new Date();
+			var totalSeconds = parseInt(this.endTime - currentTime.getTime() / 1000);
+			var hours = parseInt(totalSeconds / 3600) % 24;
+			var minutes = parseInt(totalSeconds / 60) % 60;
+			var seconds = totalSeconds % 60;
+		
+			var timeArr = [];
+			if (hours) timeArr.push(hours < 10 ? "0" + hours : hours);
+			timeArr.push(minutes < 10 ? "0" + minutes : minutes);
+			timeArr.push(seconds < 10 ? "0" + seconds : seconds);
+		
+			this.countdownString.innerHTML = timeArr.join(":");
+		},
+		startTimer: function () {
+		  var _this = this;
+			this.setTime();
+			setInterval(function () { _this.setTime.call(_this) }, 1000);
+		}
+	}
+	
+	return timer;
+})();
+
+var t = new Timer(document.getElementsByTagName('article')[0]);
+t.startTimer();
+
+
