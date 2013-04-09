240166f1210b4c2d7cd97d2f739f4ecec6aabe39
Angela Panfil <panfia1@gmail.com>
2013-04-09 11:48:59 -0400

    Re-added settings.rb using environment variables

---
+++ settings.rb
@@ -0,0 +1,16 @@
+#!usr/bin/env ruby
+
+SETTINGS = {
+    :database => {
+        :type => "postgres",
+        :user => ENV["DB_USER"],
+        :password => ENV["DB_PASSWORD"],
+        :server => "localhost",
+        :name => "presentator"
+    },
+    :presentation => {
+        :day => 1
+    }
+}
+
+puts SETTINGS.inspect
