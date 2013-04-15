bc50a2da6e105d8357fec91b55939454eb1d374e
Colin Regan <colin@redcup.la>
2013-04-15 12:02:56 -0400

    Fixed ruby shabangs

--- presentator.rb
+++ presentator.rb
@@ -1,4 +1,4 @@
-#!usr/bin/env ruby
+#!/usr/bin/env ruby
 
 require 'sinatra'
 require 'data_mapper'
--- settings.rb
+++ settings.rb
@@ -1,5 +1,3 @@
-#!usr/bin/env ruby
-
 SETTINGS = {
     :database => {
         :type => "postgres",
